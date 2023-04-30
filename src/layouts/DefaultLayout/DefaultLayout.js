04614c6e68e4d4a50e0835d63922506103998d6c
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-05-01 06:02:59 +0700

    add link to HomePage to logo

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -2,13 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./DefaultLayout.module.scss";
 import clsx from "clsx";
+import { Link } from "react-router-dom";
 
 DefaultLayout.propTypes = {};
 
 function DefaultLayout({ children }) {
   return (
     <div className={clsx(styles.wrapper)}>
-      <h1 className={clsx(styles.header)}>Rote Learning</h1>
+      <Link to={"/"} className={clsx(styles.header)}>
+        ROTE LEARNING
+      </Link>
       <div className={clsx(styles.content)}>{children}</div>
     </div>
   );
